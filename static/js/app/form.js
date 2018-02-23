fb9ea09e6db60507d55c09f352eec02d4a4ffca5
ysparknanum <ysparknanum@gmail.com>
2018-02-24 02:15:52 +0900

    d day 등록 다이어로그

--- static/js/app/form.js
+++ static/js/app/form.js
@@ -1,9 +1,63 @@
+Vue.component('form-input', {
+
+    props: ['tag', 'name'],
+
+    template: `
+        <div class="input-group-wrap">
+            <div class="input-group">
+                <label :class="isFocusLabel">{{ name }}</label>
+                <div class="input-wrap" :class="isFocusInput">
+                    <input
+                        type="text"
+                        :name="tag"
+                        @focusin="setFocus"
+                        @focusout="setFocus"
+                        @keyup="inputVal">
+                </div>
+            </div>
+        </div>
+    `,
+
+    computed: {
+
+        isFocusLabel: function() {
+            return {
+                focus: this.focus,
+                filled: this.value.length > 0
+            }
+        },
+
+        isFocusInput: function() {
+            return {
+                focus: this.focus
+            }
+        }
+    },
+
+    data: function() {
+        return {
+            focus: false,
+            value: ''
+        }
+    },
+
+    methods: {
+        setFocus: function() {
+            this.focus = !this.focus;
+        },
+
+        inputVal: function(evt) {
+            this.value = evt.target.value;
+        }
+    }
+})
+
 _formModal = new Vue({
     el: '.dialog',
 
     template: `
         <div class="dialog" :style="dialogStatus">
-            <div class="head">
+            <div class="dialog-head">
                 <div class="close-btn">
                     <i class="material-icons" @click="hide">close</i>
                 </div>
@@ -13,6 +67,12 @@ _formModal = new Vue({
                 <div class="save-btn">
                     <span>저장</span>
                 </div>
+            </div>
+            <div class="dialog-body">
+                <form-input
+                    name="D-Day 이름"
+                    tag="name"
+                />
             </div>
         </div>
     `,
